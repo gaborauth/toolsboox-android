47db2fd8af7c737caf30471c38d5c36ba5b1d581
Gábor AUTH <auth.gabor@gmail.com>
2021-01-11 22:02:30 +0100

    Fixed localization upload script.

--- poeditor_upload.sh
+++ poeditor_upload.sh
@@ -3,7 +3,9 @@
 curl -X POST https://api.poeditor.com/v2/projects/upload \
   -F api_token="$1" \
   -F id="403165" \
-  -F updating="terms" \
+  -F language="en" \
+  -F overwrite="1" \
+  -F updating="terms_translations" \
   -F sync_terms="1" \
   -F file=@"app/src/main/res/values/strings.xml"
 
