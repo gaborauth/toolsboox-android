5414025fcf020462728ed92747a65a390e50e6a8
Gábor AUTH <auth.gabor@gmail.com>
2023-01-20 15:02:18 +0100

    Updated Czech localization.

--- poeditor_download.sh
+++ poeditor_download.sh
@@ -15,6 +15,7 @@ do
     JSON=`curl -s -X POST https://api.poeditor.com/v2/projects/export -d api_token="$1" -d id="403165" -d language="$i" -d type="android_strings" -d filters="translated"`
     echo "Export URL of $i: $JSON"
     URL=`echo $JSON | jq -r .result.url`
+    echo "Export URL: $URL"
     mkdir -p app/src/main/res/values-$VALUESDIRNAME
     curl -s $URL --output app/src/main/res/values-$VALUESDIRNAME/strings.xml
     echo "Saved to app/src/main/res/values-$VALUESDIRNAME/strings.xml"
