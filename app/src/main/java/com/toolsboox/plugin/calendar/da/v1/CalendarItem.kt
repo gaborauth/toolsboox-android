89b707e1b9769d86338986e5143f4014dd2f5d9f
Gábor AUTH <auth.gabor@gmail.com>
2023-01-31 09:06:48 +0100

    Code quality improvements.

---
+++ app/src/main/java/com/toolsboox/plugin/calendar/da/v1/CalendarItem.kt
@@ -0,0 +1,16 @@
+package com.toolsboox.plugin.calendar.da.v1
+
+import java.util.*
+
+/**
+ * Calendar item in the cloud storage.
+ *
+ * @author <a href="mailto:gabor.auth@toolsboox.com">Gábor AUTH</a>
+ */
+data class CalendarItem(
+    val userId: UUID,
+    val baseName: String,
+    val version: String,
+    val created: Date?,
+    val updated: Date?,
+)
